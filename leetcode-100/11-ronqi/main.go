a226b43a00e801651935d856d47db7e9ffeccfe3
hufeng <15072754900@163.com>
2024-09-27 10:53:05 +0800

    cur 11

--- leetcode-100/11-ronqi/main.go
+++ leetcode-100/11-ronqi/main.go
@@ -3,11 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
-	fmt.Scan(a, b)
-	fmt.Println(a, b)
+	// 定义双指针，进行在取值区间内移动，计算面积，并与当前最大值比较，获取更大的一个，然后返回
+	nums := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	l := 0
+	r := len(nums) - 1
+	var ans int
+
+	for l < r {
+		cur := (r - l) * min(nums[l], nums[r])
+		ans = max(ans, cur)
+		if nums[l] <= nums[r] {
+			l++
+		} else {
+			r--
+		}
+	}
+	fmt.Println(ans)
 }
 
 // 处理力扣上11题，盛水的容器，使用的是双指针处理。
 
-// 进行gitadd的尝试
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
